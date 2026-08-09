b150b4e3131c5441ac4a939e4e9b21aa5d8863a3
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-09 02:48:04 +0100

    feat: add home page with search bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./components/SearchMenu";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full max-w-3xl flex-col items-center place-items-center justify-between py-32 px-14 bg-white dark:bg-black sm:items-start">
+      <div className="flex w-full max-w-3xl flex-col items-center place-items-center justify-between py-32 px-14 bg-white dark:bg-black sm:items-start">
         <section
           id="home-banner"
           className="flex flex-col gap-8 text-justify place-items-center px-4"
@@ -25,7 +25,7 @@ export default function Home() {
             <SearchBar />
           </div>
         </section>
-      </main>
+      </div>
     </div>
   );
 }
