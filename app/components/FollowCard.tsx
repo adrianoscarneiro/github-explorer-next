957edcf802b35d113106c3471c9feab2a4629702
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-13 23:52:42 +0100

    refactor: rename UserAvatar.tsx to FollowCard.tsx

--- app/components/FollowCard.tsx
+++ app/components/FollowCard.tsx
@@ -1,22 +1,26 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-export default function UserAvatar({
+
+export default function FollowCard({
   username,
   avatar_url,
 }: {
   username: string;
-  avatar_url: string;
+  avatar_url?: string;
 }) {
   return (
     <div className="flex sm:flex-col sm:place-items-center gap-1 text-center ">
-      <Image
-        src={avatar_url ?? null}
-        alt={`This is ${username}'s avatar`}
-        width={50}
-        height={50}
-        className="rounded-full"
-      />
       <Link href={`./${username}`}>
+        <img
+          src={avatar_url ?? null}
+          alt={`This is ${username}'s avatar`}
+          width={50}
+          height={50}
+          className="rounded-full"
+          loading="lazy"
+          decoding="async"
+        />
         <p className="truncate">{username}</p>
       </Link>
     </div>
