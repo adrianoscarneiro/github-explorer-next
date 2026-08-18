b504040b7827db407263f25ab074ac092cd7b20b
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-18 18:30:54 +0100

    refactor: Edit UI for Home page

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,18 @@
-
 import Link from "next/link";
+import Button from "./Button";
 
 export default function Home() {
   return (
-    <div className="w-full h-8 flex flex-row justify-center place-items-center bg-white text-black">
+    <nav
+      aria-label="Main navigation"
+      className="w-full h-8 grid grid-cols-1 items-center bg-white text-black px-4"
+    >
       <ul>
-        <li className="flex flex-row gap-4">
+        <li className="flex flex-row gap-4 justify-self-center">
           <Link href={"./"}>Home</Link>
           <Link href={"./"}>Search</Link>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,19 @@ import SearchBar from "./components/SearchMenu";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex w-full max-w-3xl flex-col items-center place-items-center justify-between py-32 px-14 bg-white dark:bg-black sm:items-start">
-        <section
-          id="home-banner"
-          className="flex flex-col gap-8 text-justify place-items-center px-4"
-        >
-          <div className="flex flex-col gap-4 text-justify">
-            <h1 className="text-h1 text-center">
-              <strong>GITHUB EXPLORER</strong>
-            </h1>
-
-            <p className="text-p">
-              Explore any GitHub profile in seconds. GitHub Explorer lets you
-              look up any GitHub user and see what they are building, profile
-              info, follower count, and their public repositories, all pulled
-              live from the GitHub API. Just search a username and dive in.
-            </p>
-          </div>
-
-          <div>
-            <SearchBar />
-          </div>
-        </section>
+    <section className="flex flex-col gap-2 py-8 px-4 bg-black text-white min-h-screen">
+      <div className="self-center px-2 py-4">
+        <SearchBar />
       </div>
-    </div>
+      <div className="text-center sm:flex sm:flex-col sm:place-items-center">
+        <h1 className="px-2 py-1">Github Explorer</h1>
+        <p className="px-2 py-1 sm:w-1/2">
+          Explore any GitHub profile in seconds. GitHub Explorer lets you look
+          up any GitHub user and see what they are building, profile info,
+          follower count, and their public repositories, all pulled live from
+          the GitHub API. Just search a username and dive in.
+        </p>
+      </div>
+    </section>
   );
 }
