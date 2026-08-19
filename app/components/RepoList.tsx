11d444ac7c4565803cc7ad966a6a9302811e7fe0
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-19 12:52:10 +0100

    refactor: Edit RepoList return component storing the map return in a constant

--- app/components/RepoList.tsx
+++ app/components/RepoList.tsx
@@ -4,36 +4,33 @@ import RepoCard from "./RepoCard";
 
 export default async function RepoList({ user }: { user: string }) {
   const repos: GithubRepos[] = await getGithubRepos(user);
-
-  return (
-    <div>
-      {repos.map(
-        ({
-          id,
-          name,
-          description,
-          created_at,
-          updated_at,
-          watchers_count,
-          stargazers_count,
-          language,
-        }) => {
-          return (
-            <div key={id} className="sm:w-170 bg-[#3d4757] rounded-xl">
-              <RepoCard
-                name={name}
-                description={description}
-                stargazers_count={stargazers_count}
-                watchers_count={watchers_count}
-                language={language}
-                id={id}
-                created_at={created_at}
-                updated_at={updated_at}
-              />
-            </div>
-          );
-        },
-      )}
-    </div>
+  const listRepos = repos.map(
+    ({
+      id,
+      name,
+      description,
+      created_at,
+      updated_at,
+      watchers_count,
+      stargazers_count,
+      language,
+    }) => {
+      return (
+        <div key={id} className="sm:w-170 bg-[#3d4757] rounded-xl">
+          <RepoCard
+            name={name}
+            description={description}
+            stargazers_count={stargazers_count}
+            watchers_count={watchers_count}
+            language={language}
+            id={id}
+            created_at={created_at}
+            updated_at={updated_at}
+          />
+        </div>
+      );
+    },
   );
+
+  return <div className="flex flex-col gap-2">{listRepos}</div>;
 }
