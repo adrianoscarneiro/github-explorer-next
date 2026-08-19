436047341d048bfa16bd18674b1d746cf5b1f0fb
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-19 14:40:31 +0100

    refactor: Edit UI for RepoCard

--- app/components/RepoCard.tsx
+++ app/components/RepoCard.tsx
@@ -1,6 +1,5 @@
 import { GithubRepos } from "../types/githubTypes";
 import { formatDate } from "../utils/modules";
-import Badge from "./Badge";
 
 export default function RepoCard({ repoProps }: { repoProps: GithubRepos }) {
   const {
@@ -11,25 +10,34 @@ export default function RepoCard({ repoProps }: { repoProps: GithubRepos }) {
     watchers_count,
     stargazers_count,
   } = repoProps;
+
+  const formatedDate = formatDate(updated_at);
+
   return (
-    <div className="flex flex-col px-4 py-4 sm:flex-row w-full gap-2">
-      <div className="flex flex-col sm:w-2/3 sm:h-full sm:justify-evenly gap-2 ">
-        <div className="sm:w-full justify-center">
-          <div className="flex sm:flex-row gap-2">
-            <Badge title="Language" value={language} />
-            <Badge title="Project" value={name} />
-          </div>
-        </div>
-        <div className="">
-          <Badge title="Description" value={description} />
-        </div>
+    <div className="w-full grid grid-cols-3 gap-2 grid-rows-2 px-2 py-4 justify-center sm:grid-col-5 sm:grid-rows-[1fr_2fr] sm:gap-3 ">
+      <div className="col-start-1 truncate">
+        <dt className="">Project</dt>
+        <dd className="">{name}</dd>
       </div>
-      <div className="sm:place-items-center sm:justify-center">
-        <div className="flex flex-row w-full gap-2 sm:flex-col">
-          <Badge title="Last update" value={formatDate(updated_at)} />
-          <Badge title="Watchers" value={watchers_count} />
-          <Badge title="Stars" value={stargazers_count} />
-        </div>
+      <div className="col-start-2 col-end-3 row-start-1 ">
+        <dt>Language</dt>
+        <dd>{language}</dd>
+      </div>
+      <div className="col-start-1 col-end-3 row-start-2 row-end-4 text-justify sm:row-end-2 sm:col-end-5">
+        <dt>Description</dt>
+        <dd>{description}</dd>
+      </div>
+      <div className="col-start-3 col-end-4 row-start-2 sm:row-start-1 sm:col-start-3">
+        <dt>Watchers</dt>
+        <dd>{watchers_count}</dd>
+      </div>
+      <div className="col-start-3 col-end-4 row-start-3 sm:row-start-1 sm:col-start-4">
+        <dt>Stars</dt>
+        <dd>{stargazers_count}</dd>
+      </div>
+      <div className="col-start-3 sm:row-start-1 sm:col-start-5">
+        <dt>Last update:</dt>
+        <dd>{formatedDate}</dd>
       </div>
     </div>
   );
