07142cb948181696f5fd4fbb767b396c0a9892a9
Adriano Carneiro <78058402+adrianoscarneiro@users.noreply.github.com>
2026-08-21 12:36:02 +0100

    refactor: Edit responsive properties and add dark and light classes

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -17,62 +17,38 @@ export default function UserCard({
     avatar_url,
     location,
     public_repos,
-    blog,
   } = userDetails;
   return (
-    <div className="flex flex-col secondary px-2 py-4 w-full sm:flex-col sm:gap-2 rounded-xl sm:place-items-center sm:w-110 sm:max-w-150 sm:max-h-screen outline-1 outline-white">
-      {/* Main card section*/}
-      <section className="flex flex-col gap-4 justify-center place-items-center rounded-xl ">
-        {/* Picture section*/}
+    <section className="flex flex-col gap-4 py-4 px-4 bg-card">
+      <div className="flex flex-col gap-2 place-items-center">
         <Image
           width={100}
           height={100}
           src={avatar_url}
-          className="rounded-full outline-6"
+          className="rounded-full outline-6 outline-secondary-text"
           alt={`This is ${name}'s avatar`}
         />
-        <div className="">
-          <p>{name ? name : username}</p>
-        </div>
-      </section>
-      <section className="h-full flex flex-col gap-2 place-items-center sm:max-h-full sm:flex-wrap text-center rounded-xl">
-        {/* Card details section*/}
-        <div className="flex flex-row color-black w-80 gap-6 justify-center max-w-full sm:w-auto rounded-xl">
-          <div>
-            <Badge title="Followers" value={followers} />
-          </div>
-          <div>
-            <Badge title="Following" value={following} />
-          </div>
-          <div>
-            <Badge title="Public Repos" value={public_repos} />
-          </div>
-        </div>
-        <div className="flex flex-row w-100 justify-evenly sm:gap-4 truncate sm:flex-col sm:w-50">
-          {location && (
-            <div className="">
-              <p>Location</p>
-              <p>{location}</p>
-            </div>
-          )}
+        <p className="text-primary-text">
+          <strong>{name ? name : username}</strong>
+        </p>
+      </div>
+      <div className="flex flex-row w-full px-4 text-secondary-text">
+        <Badge title="Followers" value={followers} />
+        <Badge title="Following" value={following} />
+        <Badge title="Public Repos" value={public_repos} />
+      </div>
+      <div className="flex flex-col text-secondary-text">
+        {location && (
           <div className="truncate">
-            {blog && (
-              <div>
-                <p>Personal Page</p>
-                <a href={blog}>
-                  <p>{blog}</p>
-                </a>
-              </div>
-            )}
-          </div>
-        </div>
-        {bio && (
-          <div className="w-80 sm:w-full">
-            <p>Bio</p>
-            <p>{bio}</p>
+            <Badge title="Location" value={location} />
           </div>
         )}
-      </section>
-    </div>
+      </div>
+      {bio && (
+        <div className="text-secondary-text text-justify">
+          <Badge title={"Bio"} value={bio} />
+        </div>
+      )}
+    </section>
   );
 }
